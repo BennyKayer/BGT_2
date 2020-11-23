afbd224fbe5ab0dc9ee49b610f8819a67ec9253e
Stanisław Lutkiewicz <stanislaw.lutkiewicz@softgent.com>
2020-11-23 13:26:11 +0100

    Added comment with little description and authors to main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+__author__ = "Paweł Benkowski, Stanisław Lutkiewicz"
+'''
+main.py - main file to count and print all of Friedrich Nietzsche wrote
+count_words.py - contains a function for counting words from pdf file's
+webScrapper.py - downloads all Nietzsche works from web to folder as pdf file's
+'''
+
 from pathlib import Path
 from count_words import count_words
 from webScrapper import get_books
